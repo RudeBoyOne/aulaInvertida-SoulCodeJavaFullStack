601d194b949ce4b88e68c4008079533410853111
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2022-02-24 18:19:03 -0300

    subindo arquivos finalizados

--- ana/timeout/timeout.js
+++ ana/timeout/timeout.js
@@ -3,9 +3,9 @@ function digaOi() {
   }
 digaOi()
 
-/*let myGreeting = setTimeout(function sayHi() {
-    alert('Hello, Mr. Universe!');
-  }, 3000)*/
+// let myGreeting = setTimeout(function sayHi() {
+//     alert('Hello, Mr. Universe!');
+//   }, 3000)
 
 let teste = "Se aparecer depois do Alert é síncrono e se aparecer depois é assíncrono"
 document.write(teste)
